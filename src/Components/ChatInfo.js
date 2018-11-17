db78458402c6fe91202e4bfef0184a97f949de29
권재원 <springkjw@gmail.com>
2018-11-18 07:59:36 +0900

    채팅방 CSS 변경

--- src/Components/ChatInfo.js
+++ src/Components/ChatInfo.js
@@ -4,7 +4,7 @@ import './ChatInfo.css';
 function ChatInfo(props) {
   if (props.info.is_owner) {
     return (
-      <li className="message__item"><span className="message__user">{props.info.user}</span><span className="message__text is_self">{props.info.message}</span></li>
+      <li className="message__item message__item__active"><span className="message__user">{props.info.user}</span><span className="message__text is_self">{props.info.message}</span></li>
     );
   } else {
     return (
