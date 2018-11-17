d9f98b4c860f54daa3cc7defba5ece79c9b95b0e
권재원 <springkjw@gmail.com>
2018-11-17 23:00:28 +0900

    웹 소켓 연결

--- src/Components/ChattingRoom.js
+++ src/Components/ChattingRoom.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import ChatForm from './ChatForm';
-import ChatInfoList from './ChatInfoList'
+import ChatInfoList from './ChatInfoList';
+import axios from 'axios';
 
 class ChattingRoom extends Component {
   constructor(props) {
@@ -22,9 +23,34 @@ class ChattingRoom extends Component {
     
     this.handeCreate = this.handleCreate.bind(this);
   }
+
+  componentDidMount() {
+    // 소켓 연결 전에 이전 메세지들 불러오기
+    axios.get('http://125.132.216.192:8000/api/chat/1', (res) => {
+      this.setState({
+        chattingList: res.data
+      })
+    })
+
+
+    this.socket = new WebSocket('ws://125.132.216.192:8000/chat/1');
+    if (this.socket.readyState === WebSocket.OPEN) {
+      console.log('connected!');
+    } else if (this.socket.readyState === WebSocket.CONNECTING) {
+      console.log('connecting...');
+    }
+  }
   
   handleCreate = (chat) => {
     const { chattingList } = this.state;
+
+    // 소켓이 연결되지 않은 경우 에러 메세지 띄우기
+    if (this.socket.readyState === WebSocket.OPEN) {
+      this.socket.send(JSON.stringify({
+        message: chat,
+      }));
+    }
+
     this.setState({
       chattingList: chattingList.concat({
         id: this.id++, ...chat
