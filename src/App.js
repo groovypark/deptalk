cc0e4b4c4673bdfcc90550c42822061bb9c9d7f6
groovypark <soobin950@gmail.com>
2018-11-17 21:55:29 +0900

    Update Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
+// import { Route, Switch } from "react-router-dom";
 import AddUser from './components/AddUser';
-import ChattingRoom from './components/ChattingRoom';
+// import ChattingRoom from './components/ChattingRoom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Route exact path="/" component={AddUser}/>
-        <Route path="/chatting" component={ChattingRoom}/>
-      </div>
+      <AddUser/>
+      // <Switch>
+      //   <Route exact path="/" component={AddUser}/>
+      //   <Route path="/chatting" component={ChattingRoom}/>
+      // </Switch>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+// import AddUser from './components/AddUser';
+import ChattingRoom from './components/ChattingRoom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const router = (
+  <Router>
+    <Switch>
+        <Route exact path="/" component={App}/>
+        <Route path="/chatting" component={ChattingRoom}/>
+      </Switch>
+  </Router> 
+)
+ReactDOM.render(router, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
