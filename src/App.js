3bad0e15d5c9b44a349d66d591ac9edafa4c91af
groovypark <soobin950@gmail.com>
2018-11-17 21:13:38 +0900

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
-import AddUser from './Components/AddUser';
+import { Route } from 'react-router-dom';
+import AddUser from './components/AddUser';
+import ChattingRoom from './components/ChattingRoom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <AddUser />
+        <Route exact path="/" component={AddUser}/>
+        <Route path="/chatting" component={ChattingRoom}/>
       </div>
     );
   }
