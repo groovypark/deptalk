2272acb72061f3e0a633b85dd788174b1cdf252a
r0o0 <rayeoh7@gmail.com>
2018-11-18 07:04:58 +0900

    update style

--- src/Components/ChatForm.js
+++ src/Components/ChatForm.js
@@ -3,6 +3,13 @@ import Textarea from "react-textarea-autosize";
 import './ChatForm.css';
 
 export default class ChatForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
   state = {}
   handleChange = (e) => {
     this.setState({
@@ -18,11 +25,13 @@ export default class ChatForm extends Component {
     this.props.onSubmit(this.state.text);
   }
   render() {
+    const text = this.state.text;
+    console.log(this.state);
     return (
-      <form className="chat__input_container" onSubmit={this.handleSubmit.bind(this)}>
+      <form className="chat__input_container" onSubmit={this.handleSubmit}>
         {/* </form>/<h1 value={this.state.name} onChange={this.handleChange} name="name"/> </h1> */}
-        <Textarea className="chat__input_area" value={this.state.text} onChange={this.handleChange.bind(this)} name="chat"/>
-        <button className="chat__input_button" type="submit">등록</button>
+        <Textarea className="chat__input_area" value={this.state.text} onChange={this.handleChange} name="chat"/>
+        <button className={ text !== undefined && text.length > 1  ? 'chat__input_button_active' : 'chat__input_button' } type="submit">등록</button>
       </form>
     );
   }
