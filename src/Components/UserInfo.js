ca13577396e65376ab75fddca36f6c6fd5b51d39
r0o0 <rayeoh7@gmail.com>
2018-11-18 04:48:37 +0900

    refactor code

--- src/Components/UserInfo.js
+++ src/Components/UserInfo.js
@@ -1,20 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class ChatInfo extends Component {
-  static defaultProps = {
-    info: {
-      user: '이름',
-      id: 0
-    }
-  }
-  
-  render() {
-    const {user} = this.props.info;
-    
-    return (
-      <div>
-        <div><b>{user}</b></div>
-      </div>
-    );
-  }
+function UserInfo(props) {
+  return (
+    <li className="user__list"><span className="user__name">{props.info.user}</span></li>
+  );
 }
+
+export default UserInfo;
